7717539dccd58c2b40213d24ea99cfe1f22873a5
Joseph Phillips <josephephillips27@gmail.com>
2014-10-29 22:07:11 -0400

    Fresh Start

---
+++ JoesComicShopv2.Application/JoesComicShopv2ApplicationModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Abp.Dependency;
+using Abp.Modules;
+using Abp.Startup;
+using Abp.Startup.Application;
+
+namespace JoesComicShopv2
+{
+    public class JoesComicShopv2ApplicationModule : AbpModule
+    {
+        public override Type[] GetDependedModules()
+        {
+            return new[]
+                   {
+                       typeof(AbpApplicationModule),
+                       typeof(JoesComicShopv2CoreModule)
+                   };
+        }
+
+        public override void Initialize(IAbpInitializationContext initializationContext)
+        {
+            base.Initialize(initializationContext);
+            IocManager.Instance.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ JoesComicShopv2.Infrastructure.EntityFramework/EntityFramework/Repositories/JoesComicShopv2RepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories.EntityFramework;
+
+namespace JoesComicShopv2.EntityFramework.Repositories
+{
+    public abstract class JoesComicShopv2RepositoryBase<TEntity, TPrimaryKey> : EfRepositoryBase<JoesComicShopv2DbContext, TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+    }
+
+    public abstract class JoesComicShopv2RepositoryBase<TEntity> : JoesComicShopv2RepositoryBase<TEntity, int>
+        where TEntity : class, IEntity<int>
+    {
+
+    }
+}
