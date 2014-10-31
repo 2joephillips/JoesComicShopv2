db7dbf15cba0b1ef9116356ede15f52506224fe9
Joseph Phillips <josephephillips27@gmail.com>
2014-10-30 23:08:52 -0400

    Task Repository

---
+++ JoesComicShopv2.Infrastructure.EntityFramework/EntityFramework/Repositories/TaskRepository.cs
@@ -0,0 +1,43 @@
+﻿using JoesComicShopv2.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JoesComicShopv2.Tasks;
+
+namespace JoesComicShopv2.EntityFramework.Repositories
+{
+
+    /// <summary>
+    /// Implements <see cref="ITaskRepository"/> for EntityFramework ORM.
+    /// </summary>
+    class TaskRepository : JoesComicShopv2RepositoryBase<Task, long>, ITaskRepository
+    {
+        public List<Task> GetAllWithPeople(int? assignedPersonId, TaskState? state)
+        {
+            //In repository methods, we do not deal with create/dispose DB connections, DbContexes and transactions. ABP handles it.
+
+            var query = GetAll(); //GetAll() returns IQueryable<T>, so we can query over it.
+            //var query = Context.Tasks.AsQueryable(); //Alternatively, we can directly use EF's DbContext object.
+            //var query = Table.AsQueryable(); //Another alternative: We can directly use 'Table' property instead of 'Context.Tasks', they are identical.
+
+            //Add some Where conditions...
+
+            if (assignedPersonId.HasValue)
+            {
+                query = query.Where(task => task.AssignedPerson.Id == assignedPersonId.Value);
+            }
+
+            if (state.HasValue)
+            {
+                query = query.Where(task => task.State == state);
+            }
+
+            return query
+                .OrderByDescending(task => task.CreationTime)
+                .Include(task => task.AssignedPerson) //Include assigned person in a single query
+                .ToList();
+        }
+
+    }
+}
